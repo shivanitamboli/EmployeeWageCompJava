ae1d45f1a532aaed3dfdfb520cf953c92efb8535
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-18 15:44:47 +0530

    first commit

---
+++ src/com/bridgelabz/EmployeeWage.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+       System.out.println("welcome to employee wage computation problem");
+	}
+
+}
