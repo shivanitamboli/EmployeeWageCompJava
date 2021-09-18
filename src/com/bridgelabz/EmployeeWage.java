08abac5916efa4ef6bd2e1154fa0caf308ec074e
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-18 15:58:24 +0530

    Added : employee is present or absent using random

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,7 +3,19 @@ package com.bridgelabz;
 public class EmployeeWage {
 
 	public static void main(String[] args) {
-       System.out.println("welcome to employee wage computation problem");
-	}
+	    int IS_PRESENT = 0;
+	    System.out.println("welcome to employee wage problem practice set");
+	    double empCheck = Math.floor(Math.random()* 10) %2;
+	    // Using random class to Get random number
+	    // math.floor returns largest double value
+	    if (empCheck == IS_PRESENT)
+	    {
+	        System.out.println("Employee is Present");
+	    }
+	    else 
+	    {
+	        System.out.println("Employee is Absent");
+	    }
 
+	}
 }
